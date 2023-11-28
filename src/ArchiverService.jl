e802a090d8f9fffe4ab9c77936d0d7a24a0a7406
William Thompson <wthompson@uvic.ca>
2023-11-28 14:29:35 -0800

    Turn on and off recording remotely

--- src/ArchiverService.jl
+++ src/ArchiverService.jl
@@ -17,6 +17,12 @@ using SpidersMessageEncoding
 
 # The full message goes onto disk. We roll over the files periodically.
 
+Base.@kwdef mutable struct PendingCmd
+    stream::Int=0
+    uri::String="aeron:ipc"
+    enabled::Bool=false
+end
+
 function main(ARGS)
 
     # SQL index-table bulk insert size. 10k is recommended.
@@ -37,14 +43,21 @@ function main(ARGS)
     
     ctx = AeronContext()
 
-    # TODO: list of multiple streams configured somewhere
-    # TODO: do we want to toggle listening to a given stream on and off while running?
+    # Dictionary of aeron subscriptions we are actively recording
+    # from.
+    aeron_stream_recording_dict = Dict{
+        Tuple{String,Int},
+        Aeron.AeronSubscription
+    }()
+
+    # This configuration object, on the other hand,
+    # is the input channel that where we listen for commands about
+    # *this* service, ie turning recording of other services on and off.
     conf = AeronConfig(
         uri="aeron:ipc",
         stream=1006
     )
 
-    confs = [conf]
 
     index_db_fname = gen_index_db_filename()
     index_db_fname_dir = joinpath(dir, index_db_fname)
@@ -93,113 +106,172 @@ function main(ARGS)
     ), chunk_N)
 
 
-    # TODO: we can't wait too long after subscribing before we start consuming!
-    # Do this at the last moment, and preferably after all code is compiled.
-    subscriptions = map(confs) do conf
-        Aeron.subscriber(ctx, conf)
-    end
 
-    # we use a cstatic string here which is just a view over bytes
-    # This is because we have to put it in the datatable repeatedly
-    current_data_fname = gen_rawdata_filename()
-    current_data_fname_dir = joinpath(dir, current_data_fname)
-    current_data_fname_c = convert(CStaticString{32}, current_data_fname)
-    
 
-    # When actually opening a file, we make it into a normal string
-    data_file = open(current_data_fname_dir, write=true)
+    # To turn on / off recording, send `uri=<uri>`, then `stream=<stream_no>`, 
+    # then `enabled=1` or `enabled=0`, and finally a commit all with the same
+    # correlation number.
+    pending_commands_by_corr_id = Dict{Int,PendingCmd}()
+        
+    Aeron.subscriber(ctx, conf) do control_subscription
 
-    # We increment row_i for each message received. 
-    # Once we reach chunk_N, we send to the DB and resume.
-    row_i = 0
-    last_commit_time = time()
+        # Would be more appropriate to allocate these earlier in the code
+        # but we trigger the Julia slow closure issue.
 
-    # Data_i is the end of the data in the raw file on disk.
-    # This will be added to the message index table as an offset
-    data_i = 0 
 
-    GC.enable_logging(true)
+        # we use a cstatic string here which is just a view over bytes
+        # This is because we have to put it in the datatable repeatedly
+        current_data_fname = gen_rawdata_filename()
+        current_data_fname_dir = joinpath(dir, current_data_fname)
+        current_data_fname_c = convert(CStaticString{32}, current_data_fname)
+        
 
-    try
-        while true
-            # Periodically do a bulk insert of the data index into the DB.
-            # TODO: perhaps I only have to check this periodically, even though 
-            # we want to poll very fast.
-            t = time()
-            # Commit if we are at the batch sized limit
-            if row_i == chunk_N ||
-                # Or, if we have data waiting, 
-                ( row_i > 0 && 
-                  # It's been more than commit_delay_timeout seconds
-                  # This way we don't leave data sitting in memory for long
-                  # periods of time if there's not much coming in.
-                  last_commit_time + commit_delay_timeout < t
-                )
-                # The call to load! allocates memory so occaisionally
-                # well hit a GC here. Thankfully, these GC pauses are 
-                # usually a few tens of ms, so of a similar cost to 
-                # the data loading itself.
-                println("committing $row_i message receipt(s) to index database.")
-                @time SQLite.load!(@view(bulk_insert_table[begin:row_i]), db, "message_index")
-                row_i = 0
-                last_commit_time = t
-            end
-            # TODO: periodically roll over the raw data file so it doesn't grow to large
-            if data_i > data_archive_rollover_filesize_bytes
-                println("Rolling over to new data archive.")
-                println("Closing $current_data_fname")
-                close(data_file)
-                current_data_fname = gen_rawdata_filename()
-                current_data_fname_dir = joinpath(dir, current_data_fname)
-                current_data_fname_c = convert(CStaticString{32}, current_data_fname)
-                println("New file will be $current_data_fname")
-                data_file = open(current_data_fname_dir, write=true)
-                data_i = 0
-            end
-            for sub in subscriptions
-                fragments, data = Aeron.poll(sub)
-                if !isnothing(data)
-                    begin
-                        row_i_this = row_i + 1
-                        msg = GenericMessage(data.buffer; initialize=false) # Don't clobber schemaId etc.
-                        # We'd rather not send strings containing NULL to SQlite as it will
-                        # store as BLOB instead of TEXT.
-                        # This trick gets around it.
-                        @time desc_no_nulls = view(msg.header.description, 1:lastindex(msg.header.description))
-                        bulk_insert_table[row_i_this] = (;
-                            msg.header.TimestampNs,
-                            msg.header.correlationId,
-                            description=desc_no_nulls,
-                            msg.messageHeader.schemaId,
-                            msg.messageHeader.templateId,
-                            msg.messageHeader.blockLength,
-                            msg.messageHeader.version,
-                            msg.header.channelRcvTimestampNs,
-                            msg.header.channelSndTimestampNs,
-                            data_fname=current_data_fname,
-                            data_start_index = data_i
-                        )
-                        # display(SpidersMessageEncoding.sbedecode(data.buffer))
-                        write(data_file, data.buffer)
-                        data_i += length(data.buffer)
-                        row_i = row_i_this
+        # When actually opening a file, we make it into a normal string
+        data_file = open(current_data_fname_dir, write=true)
+
+        # We increment row_i for each message received. 
+        # Once we reach chunk_N, we send to the DB and resume.
+        row_i = 0
+        last_commit_time = time()
+
+        # Data_i is the end of the data in the raw file on disk.
+        # This will be added to the message index table as an offset
+        data_i = 0 
+
+        try
+
+
+            GC.enable_logging(true)
+            while true
+
+                # Poll for incoming commands to *this* service
+                fragments, frame = Aeron.poll(control_subscription)
+                if !isnothing(frame)
+                    msgtype = SpidersMessageEncoding.sbemessagename(frame.buffer)
+                    if msgtype == :CommandMessage
+                        # Process command
+                        cmd = CommandMessage(frame.buffer, initialize=false)
+                        if !haskey(pending_commands_by_corr_id, cmd.header.correlationId)
+                            pending_commands_by_corr_id[cmd.header.correlationId] = PendingCmd()
+                        end
+                        pending_cmd = pending_commands_by_corr_id[cmd.header.correlationId]
+                        if cmd.command == "uri"
+                            pending_cmd.uri = getargument(String, cmd)
+                        elseif cmd.command == "stream"
+                            pending_cmd.stream = getargument(Float64, cmd)
+                        elseif cmd.command == "enabled"
+                            pending_cmd.enabled = getargument(Float64, cmd) != 0
+                        else
+                            @warn "Received unsupported command" cmd.command maxlog=1
+                        end
+                    elseif msgtype == :CommitMessage
+                        cmd = CommitMessage(frame.buffer, initialize=false)
+                        pending_cmd = pending_commands_by_corr_id[cmd.header.correlationId]
+                        if pending_cmd.stream == 0
+                            @warn "can't subscribe without setting a non-zero stream number"
+                        end
+                        key = (pending_cmd.uri, pending_cmd.stream)
+                        if pending_cmd.enabled
+                            println("Opening subscription $key")
+                            conf = AeronConfig(;pending_cmd.uri, pending_cmd.stream)
+                            aeron_stream_recording_dict[key] = Aeron.subscriber(ctx,conf)
+                        elseif haskey(aeron_stream_recording_dict, key)
+                            sub = aeron_stream_recording_dict[key] 
+                            close(sub)
+                            delete!(aeron_stream_recording_dict, key)
+                            println("Closed subscription $key")
+                        end
                     end
                 end
+                # If we're receiving a command, process it ASAP 
+                if fragments > 0
+                    continue
+                end
+
+                # Periodically do a bulk insert of the data index into the DB.
+                # TODO: perhaps I only have to check this periodically, even though 
+                # we want to poll very fast.
+                t = time()
+                # Commit if we are at the batch sized limit
+                if row_i == chunk_N ||
+                    # Or, if we have data waiting, 
+                    ( row_i > 0 && 
+                    # It's been more than commit_delay_timeout seconds
+                    # This way we don't leave data sitting in memory for long
+                    # periods of time if there's not much coming in.
+                    last_commit_time + commit_delay_timeout < t
+                    )
+                    # The call to load! allocates memory so occaisionally
+                    # well hit a GC here. Thankfully, these GC pauses are 
+                    # usually a few tens of ms, so of a similar cost to 
+                    # the data loading itself.
+                    println("committing $row_i message receipt(s) to index database.")
+                    @time SQLite.load!(@view(bulk_insert_table[begin:row_i]), db, "message_index")
+                    row_i = 0
+                    last_commit_time = t
+                end
+                # Periodically roll over the raw data file so it doesn't grow to large
+                if data_i > data_archive_rollover_filesize_bytes
+                    println("Rolling over to new data archive.")
+                    println("Closing $current_data_fname")
+                    close(data_file)
+                    current_data_fname = gen_rawdata_filename()
+                    current_data_fname_dir = joinpath(dir, current_data_fname)
+                    current_data_fname_c = convert(CStaticString{32}, current_data_fname)
+                    println("New file will be $current_data_fname")
+                    data_file = open(current_data_fname_dir, write=true)
+                    data_i = 0
+                end
+
+
+                # Loop and poll all recording subscriptions
+                for (_, sub) in aeron_stream_recording_dict
+                    fragments, data = Aeron.poll(sub)
+                    if !isnothing(data)
+                        begin
+                            row_i_this = row_i + 1
+                            msg = GenericMessage(data.buffer; initialize=false) # Don't clobber schemaId etc.
+                            # We'd rather not send strings containing NULL to SQlite as it will
+                            # store as BLOB instead of TEXT.
+                            # This trick gets around it.
+                            desc_no_nulls = view(msg.header.description, 1:lastindex(msg.header.description))
+                            bulk_insert_table[row_i_this] = (;
+                                msg.header.TimestampNs,
+                                msg.header.correlationId,
+                                description=desc_no_nulls,
+                                msg.messageHeader.schemaId,
+                                msg.messageHeader.templateId,
+                                msg.messageHeader.blockLength,
+                                msg.messageHeader.version,
+                                msg.header.channelRcvTimestampNs,
+                                msg.header.channelSndTimestampNs,
+                                data_fname=current_data_fname,
+                                data_start_index = data_i
+                            )
+                            # display(SpidersMessageEncoding.sbedecode(data.buffer))
+                            write(data_file, data.buffer)
+                            data_i += length(data.buffer)
+                            row_i = row_i_this
+                        end
+                    end
+                end
+                GC.safepoint() # To allow receiving SIGINT (CTRL+C)
+            end # End while-poll loop.
+        finally
+            for (_, sub) in aeron_stream_recording_dict
+                close(sub)
             end
-            GC.safepoint() # To allow receiving SIGINT (CTRL+C)
-            # TODO: file roll-over
+            # Error, only commit up to the last message and not any junk left after it
+            if 0 < row_i <= chunk_N
+                println("committing partial results to table.")
+                @time SQLite.load!(@view(bulk_insert_table[1:row_i]), db, "message_index")
+                row_i = 0
+            end
         end
-    finally
-        # Error, only commit up to the last message and not any junk left after it
-        if 0 < row_i <= chunk_N
-            println("committing partial results to table.")
-            @time SQLite.load!(@view(bulk_insert_table[1:row_i]), db, "message_index")
-            row_i = 0
-        end
-    end
+    end # End Aeron-control subscription
+
     close(db)
     close(ctx)
-    
 end
 
 function gen_index_db_filename()
